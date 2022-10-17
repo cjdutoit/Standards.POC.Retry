199e34b01eb227072dbb300bef2dc8afabaaeb13
Christo du Toit <christo@dutoit.co.uk>
2022-10-17 22:18:23 +0100

    BROKERS: Logging - Log LogCritical

--- Standards.POC.Retry.Api/Brokers/Loggings/LoggingBroker.cs
+++ Standards.POC.Retry.Api/Brokers/Loggings/LoggingBroker.cs
@@ -24,5 +24,8 @@ namespace Standards.POC.Retry.Api.Brokers.Loggings
 
         public void LogError(Exception exception) =>
             this.logger.LogError(exception.Message, exception);
+
+        public void LogCritical(Exception exception) =>
+            this.logger.LogCritical(exception, exception.Message);
     }
 }
