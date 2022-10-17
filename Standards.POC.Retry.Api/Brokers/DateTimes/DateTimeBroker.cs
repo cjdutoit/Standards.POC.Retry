0877d41bb0f0beb6704473f74fe47c2affe121e4
Christo du Toit <christo@dutoit.co.uk>
2022-10-17 22:17:12 +0100

    BROKERS: DateTime - Get Current DateTime Offset

---
+++ Standards.POC.Retry.Api/Brokers/DateTimes/DateTimeBroker.cs
@@ -0,0 +1,10 @@
+using System;
+
+namespace Standards.POC.Retry.Api.Brokers.DateTimes
+{
+    public class DateTimeBroker : IDateTimeBroker
+    {
+        public DateTimeOffset GetCurrentDateTimeOffset() =>
+            DateTimeOffset.UtcNow;
+    }
+}
