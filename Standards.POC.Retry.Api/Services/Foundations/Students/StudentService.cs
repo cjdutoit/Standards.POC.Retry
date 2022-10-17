c09fe0cbc4aaecb3d408f0d142016f12e9c551f1
Christo du Toit <christo@dutoit.co.uk>
2022-10-17 22:22:56 +0100

    ShouldThrowNotFoundExceptionOnRetrieveByIdIfStudentIsNotFoundAndLogItAsync -> PASS

--- Standards.POC.Retry.Api/Services/Foundations/Students/StudentService.cs
+++ Standards.POC.Retry.Api/Services/Foundations/Students/StudentService.cs
@@ -43,6 +43,8 @@ namespace Standards.POC.Retry.Api.Services.Foundations.Students
                 Student maybeStudent = await this.storageBroker
                     .SelectStudentByIdAsync(studentId);
 
+                ValidateStorageStudent(maybeStudent, studentId);
+
                 return maybeStudent;
             });
     }
